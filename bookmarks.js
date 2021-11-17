a51d5db957c5d34d3efceb7c76b10efd9a01c2a1
null-integer <72236550+null-integer@users.noreply.github.com>
2021-11-16 20:51:20 -0600

    Add files via upload

---
+++ bookmarks.js
@@ -0,0 +1,24 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "school",
+    links: [
+      { name: "blackboard", url: "https://mycourses.utrgv.edu/" },
+      { name: "outlook", url: "https://outlook.office365.com/mail/inbox" },
+      { name: "codio", url: "https://codio.com/home/student" },
+	  {name:  "stats", url: "https://www.pearson.com/mylab"},
+	  {name:  "edfinity", url: "https://edfinity.com/"},
+    ],
+  },
+  {
+    title: "my stuff",
+    links: [
+      { name: "youtube", url: "https://youtube.com" },
+      { name: "gmail", url: "https://mail.google.com/mail/u/0/#inbox" },
+      { name: "drive", url: "https://drive.google.com/drive/my-drive" },
+      {name:  "reddit", url: "https://www.reddit.com/",},
+	  {name:  "twitch", url: "https://www.twitch.tv/",},
+	  {name:  "monkeytype", url: "https://monkeytype.com/",},
+    ],
+  },
+];
